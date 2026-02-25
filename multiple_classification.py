b3de4af8d52cba1b8026852e8c94af77645b2fd5
kengmorant12 <1166105050189@mail.rmutt.ac.th>
2026-02-25 16:37:39 +0700

    Update multiple_classification.py

--- multiple_classification.py
+++ multiple_classification.py
@@ -58,41 +58,18 @@ default_map = {
 if(selected == 'BMI'):
     st.title('BMI Classification')
     
-    person_age = st.text_input('person_age')
     person_gender = st.selectbox('person_gender', gender_map)
-    person_education = st.selectbox('person_education', education_map)
-    person_income = st.text_input('person_income') 
-    person_emp_exp = st.text_input('person_emp_exp')
-    person_home_ownership = st.selectbox('person_home_ownership', home_map)
-    loan_amnt = st.text_input('loan_amnt')
-    loan_intent = st.selectbox('loan_intent', intent_map)
-    loan_int_rate = st.text_input('loan_int_rate')
-    loan_percent_income = st.text_input('loan_percent_income')
-    cb_person_cred_hist_length = st.text_input('cb_person_cred_hist_length')
-    credit_score = st.text_input('credit_score')
-    previous_loan_defaults_on_file = st.selectbox(
-        'previous_loan_defaults_on_file',
-        default_map)
+    person_height = st.text_input('person_height') 
+    person_weight = st.text_input('person_weight')
     
     loan_prediction = ''
     
     if st.button('Predict'):
         loan_prediction = loan_model.predict([
             [
-                float(person_age),
                 gender_map[person_gender],
-                education_map[person_education],
-                float(person_income),
-                float(person_emp_exp),
-                home_map[person_home_ownership],
-                float(loan_amnt),
-                intent_map[loan_intent],
-                float(loan_int_rate),
-                float(loan_percent_income),
-                float(cb_person_cred_hist_length),
-                float(credit_score),
-                default_map[previous_loan_defaults_on_file]
-            ]
+                float(person_height),
+                float(person_weight),
         ])
         
         if (loan_prediction[0] == 0):
@@ -178,5 +155,6 @@ if(selected == 'Riding'):
           
 
     st.success(Riding_prediction)
+
 
 
